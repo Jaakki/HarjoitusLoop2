4ce1d4659f62e4101b4400595a5ad8266e5d35f0
Jaakki <62286646+Jaakki@users.noreply.github.com>
2020-04-07 10:57:41 +0300

    Add project files.

---
+++ HarjoitusLoop2/Comic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HarjoitusLoop2
+{
+    class Comic
+    {
+        public string name;
+        public int price;
+        public string publisher;
+        public int issueNumber;
+
+        public Comic()
+        {
+
+        }
+
+        public Comic(string nname, int pprice, string ppublisher, int iissueNumber)
+        {
+            this.name = nname;
+            this.price = pprice;
+            this.publisher = ppublisher;
+            this.issueNumber = iissueNumber;
+        }
+
+    }
+}
---
+++ HarjoitusLoop2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HarjoitusLoop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comic bat1 = new Comic();
+            bat1.name = "Bat-Man";
+            bat1.price = 3;
+            bat1.publisher = "DC Comics";
+            bat1.issueNumber = 20;
+
+            Comic bat2 = new Comic();
+            bat2.name = "Bat-Man";
+            bat2.price = 3;
+            bat2.publisher = "DC Comics";
+            bat2.issueNumber = 24;
+
+            Comic bat3 = new Comic();
+            bat3.name = "Bat-Man";
+            bat3.price = 3;
+            bat3.publisher = "DC Comics";
+            bat3.issueNumber = 27;
+
+            List<Comic> comics = new List<Comic>();
+            comics.Add(bat1);
+            comics.Add(bat2);
+            comics.Add(bat3);
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine($"{bat1.name} {bat1.price} {bat1.publisher} {bat1.issueNumber}");
+                Console.WriteLine($"{bat2.name} {bat2.price} {bat2.publisher} {bat2.issueNumber}");
+                Console.WriteLine($"{bat3.name} {bat3.price} {bat3.publisher} {bat3.issueNumber}");
+            }
+
+            for (int i = 1; i < 13; i++)
+            {
+                Comic spider = new Comic("Spider-Man", 4, "Marvel", i);
+                comics.Add(spider);
+            }
+
+            for (int i = 1; i < 13; i++)
+            {
+                Comic hulk = new Comic("Incredible Hulk", 2, "Marvel", i);
+                comics.Add(hulk);
+            }
+
+            //for (int i = 0; i < comics.Count; i++)
+            //{
+            //    Console.WriteLine($"{comics[i].name} | {comics[i].publisher} | issue:{comics[i].issueNumber} | {comics[i].price}$");
+            //}
+
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < comics.Count; i++)
+            {
+                y = i+1;
+                x += comics[i].price;
+                Console.WriteLine($"{comics[i].name} | {comics[i].publisher} | issue:{comics[i].issueNumber} | {comics[i].price}$");
+            }
+            Console.WriteLine($"{x}");
+            Console.WriteLine($"{x / y}");
+        }
+    }
+}
